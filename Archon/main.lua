55ed626fac3120d62265c5712a199ae0a0c8c507
oleg6785999-lang <oleg6785999@gmail.com>
2026-05-10 03:21:49 +0300

    Update main.lua

--- Archon/main.lua
+++ Archon/main.lua
@@ -1,1 +1,38 @@
+local Engine = require(script.core.engine)
+local PlayerService = require(script.services.playerservice)
+local CameraService = require(script.services.cameraservice)
+local CharacterService = require(script.services.characterservice)
+local TargetService = require(script.services.targetservice)
+local RaycastService = require(script.services.raycastservice)
+local ConfigService = require(script.services.configservice)
+local Aimbot = require(script.modules.aimbot.orchestrator)
+local SilentAim = require(script.modules.silentaim)
+local Visuals = require(script.modules.visuals)
+local AntiAim = require(script.modules.antiaim.orchestrator)
+local Exploits = require(script.modules.exploits.orchestrator)
+local UIManager = require(script.ui.uimanager)
 
+local engine = Engine.new()
+
+engine:registerModule("PlayerService", PlayerService.new, {}, {})
+engine:registerModule("CameraService", CameraService.new, {}, {})
+engine:registerModule("CharacterService", CharacterService.new, {}, {})
+engine:registerModule("TargetService", TargetService.new, {}, {})
+engine:registerModule("RaycastService", RaycastService.new, {}, {})
+engine:registerModule("ConfigService", ConfigService.new, {"Engine"}, {})
+engine:registerModule("Aimbot", Aimbot.new, {"CameraService", "PlayerService", "CharacterService", "TargetService", "RaycastService"}, { Update = 30, Render = 50 })
+engine:registerModule("SilentAim", SilentAim.new, {"TargetService", "CharacterService", "RaycastService"}, {})
+engine:registerModule("Visuals", Visuals.new, {"CameraService", "PlayerService", "TargetService"}, { Render = 60 })
+engine:registerModule("AntiAim", AntiAim.new, {"CharacterService", "CameraService", "RaycastService"}, { Update = 20, Render = 55 })
+engine:registerModule("Exploits", Exploits.new, {"CharacterService", "PlayerService", "CameraService", "RaycastService"}, { Update = 10 })
+engine:registerModule("UIManager", UIManager.new, {"ConfigService"}, {})
+
+engine.di:resolve("ConfigService"):load(require(script.config.defaults))
+
+engine:start()
+
+engine.di:resolve("UIManager"):Start()
+
+script.Destroying:Connect(function()
+    engine:stop()
+end)
